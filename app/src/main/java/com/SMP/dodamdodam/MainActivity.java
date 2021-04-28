ec3dd2f8e48a4a7f3b4bdf728dc34c5f7a626b47
dkekzhs <dkekzhs@naver.com>
2021-04-28 15:22:52 +0900

    로그인이랑 회원가입 액티비티만 생성

--- app/src/main/java/com/SMP/dodamdodam/MainActivity.java
+++ app/src/main/java/com/SMP/dodamdodam/MainActivity.java
@@ -2,6 +2,7 @@ package com.SMP.dodamdodam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent= new Intent(getApplicationContext(),loginActivity.class); //로그인 액티비티 볼려고 설정해논거
+        startActivity(intent);
+
     }
 }
---
+++ app/src/main/java/com/SMP/dodamdodam/RegisterActvitiy.java
@@ -0,0 +1,14 @@
+package com.SMP.dodamdodam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActvitiy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
