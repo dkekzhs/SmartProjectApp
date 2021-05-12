333988ac3a7ad7ee48e41a377fd517758f5ac07e
DongJae <dkekzhs5@naver.com>
2021-05-12 22:53:21 +0900

    합침

---
+++ app/src/main/java/com/SMP/dodamdodam/Activity/infoUserActivity.java
@@ -0,0 +1,16 @@
+package com.SMP.dodamdodam.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.SMP.dodamdodam.R;
+
+public class infoUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_user);
+    }
+}
