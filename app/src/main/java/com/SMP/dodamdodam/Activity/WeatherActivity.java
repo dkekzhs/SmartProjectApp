6c77156bbbac9e18bc6e941a50d11875a88b90a5
hanbinseo <shb204@gmail.com>
2021-06-03 14:55:42 +0900

    날씨 API

---
+++ app/src/main/java/com/SMP/dodamdodam/Activity/WeatherActivity.java
@@ -0,0 +1,114 @@
+package com.SMP.dodamdodam.Activity;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.WorkerThread;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.SMP.dodamdodam.R;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import io.reactivex.rxjava3.core.Flowable;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class WeatherActivity extends AppCompatActivity {
+    TextView weatherView, temView;
+    Button getWeather;
+
+    static RequestQueue requestQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+
+        weatherView = findViewById(R.id.weatherView);
+        temView = findViewById(R.id.temView);
+        getWeather = findViewById(R.id.getAPI);
+        getWeather.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CurrentWeatherCall();
+            }
+        });
+    }
+
+    private void CurrentWeatherCall() {
+        String city = "seoul";
+        String url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=e5684e67f5cda17d9c49b43ec08464c4\n";
+
+        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+
+                    //날씨 키값 받기
+                    JSONArray weatherJson = jsonObject.getJSONArray("weather");
+                    JSONObject weatherObj = weatherJson.getJSONObject(0);
+
+                    String weather = weatherObj.getString("description");
+
+                    weatherView.setText(weather);
+
+                    //기온 키값 받기
+                    JSONObject tempK = new JSONObject(jsonObject.getString("main"));
+
+                    //기온 받고 켈빈 온도를 섭씨 온도로 변경
+                    double tempDo = (Math.round((tempK.getDouble("temp") - 273.15) * 100) / 100.0);
+                    temView.setText(tempDo + "°C");
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                return params;
+            }
+        };
+
+        request.setShouldCache(false);
+        requestQueue.add(request);
+    }
+}
