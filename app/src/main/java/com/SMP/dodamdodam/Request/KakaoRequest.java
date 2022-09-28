616974b6a4c1da818fb20857e1280ec68d5e6a83
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2022-09-28 19:41:42 +0900

    Update KakaoRequest.java
    Ip

--- app/src/main/java/com/SMP/dodamdodam/Request/KakaoRequest.java
+++ app/src/main/java/com/SMP/dodamdodam/Request/KakaoRequest.java
@@ -11,7 +11,7 @@ import java.util.Map;
 
 public class KakaoRequest extends StringRequest {
     //서버 URL 설정(php 파일 연동)
-    final static private String URL = "http://ec2-52-79-44-86.ap-northeast-2.compute.amazonaws.com/kakaologin.php";
+    final static private String URL = "localhost/kakaologin.php";
     private static final String TAG = "testlog?";
     private Map<String, String> map;
 
