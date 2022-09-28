1edae323e376f5a30bce38d2282eb9df843ff295
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2022-09-28 19:41:24 +0900

    Update WalkRequest.java
    Ip

--- app/src/main/java/com/SMP/dodamdodam/Request/WalkRequest.java
+++ app/src/main/java/com/SMP/dodamdodam/Request/WalkRequest.java
@@ -14,7 +14,7 @@ import static android.content.ContentValues.TAG;
 
 public class WalkRequest extends StringRequest {
     //서버 url 설정(php파일 연동)
-    final static  private String URL="http://ec2-52-79-44-86.ap-northeast-2.compute.amazonaws.com/WalkCount.php";
+    final static  private String URL="localhost/WalkCount.php";
     private Map<String, String> map;
 
     public WalkRequest(String UserEmail, String Date, String WalkCount, Response.Listener<String> listener){
