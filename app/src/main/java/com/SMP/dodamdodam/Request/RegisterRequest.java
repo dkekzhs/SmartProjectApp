0e6296d3f0e84009eebe30497b41e5c5645d4de1
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2022-09-28 19:43:11 +0900

    Update RegisterRequest.java

--- app/src/main/java/com/SMP/dodamdodam/Request/RegisterRequest.java
+++ app/src/main/java/com/SMP/dodamdodam/Request/RegisterRequest.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class RegisterRequest extends StringRequest {
 
     //서버 URL 설정(php 파일 연동)
-    final static private String URL = "http://ec2-52-79-44-86.ap-northeast-2.compute.amazonaws.com/Register.php";
+    final static private String URL = "localhost/Register.php";
     private Map<String, String> map;
     //private Map<String, String>parameters;
 
