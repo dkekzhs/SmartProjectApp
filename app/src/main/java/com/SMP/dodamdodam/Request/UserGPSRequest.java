46be962b74bc5d5e6ae19219c8e22f37aea08a54
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2022-09-28 19:43:23 +0900

    Update UserGPSRequest.java

--- app/src/main/java/com/SMP/dodamdodam/Request/UserGPSRequest.java
+++ app/src/main/java/com/SMP/dodamdodam/Request/UserGPSRequest.java
@@ -11,7 +11,7 @@ import java.util.Map;
 public class UserGPSRequest extends StringRequest {
 
     //서버 URL 설정(php 파일 연동)
-    final static private String URL = "http://ec2-52-79-44-86.ap-northeast-2.compute.amazonaws.com/userLoc.php";
+    final static private String URL = "localhost/userLoc.php";
     private static final String TAG = "testlog?";
     private Map<String, String> map;
 
